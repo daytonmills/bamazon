b07e9194a519364715322254d0473748ae0f9f78
Dayton Mills <dayton@statscraft.net>
2017-09-21 19:54:16 -0700

    View Products and Inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -22,26 +22,50 @@ var manager = {
     {
         console.log(chalk.bgRed.black.bold('\n  Bamazon Manager  \n'));
         inquirer.prompt([{
-            message: "Manager Main Menu",
-            type: "list",
-            choices: ["View Products For Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
-            name: "action"
+            message: 'Manager Main Menu',
+            type: 'list',
+            choices: ['View Products For Sale', 'View Low Inventory', 'Add New Product', 'Add To Inventory'],
+            name: 'action'
         }]).then((options) => {
             switch (options.action)
             {
-                case "View Products For Sale":
-                    viewProducts();
+                case 'View Products For Sale':
+                    this.viewProducts();
                     break;
-                case "View Low Inventory":
-                    viewInventory();
+                case 'View Low Inventory':
+                    this.viewInventory();
                     break;
-                case "Add to Inventory":
-                    addInventory();
+                case 'Add New Product':
+                    this.addProduct();
                     break;
-                case "Add New Product":
-                    addProduct();
+                case 'Add To Inventory':
+                    this.addInventory();
                     break;
             }
         });
-    }
+    },
+
+    viewProducts: function(callback)
+    {
+        connection.query('SELECT * FROM products', (error, products) =>
+        {
+            console.table(products);
+            if (callback) callback();
+            connection.end();
+        });
+    },
+
+    viewInventory: function()
+    {
+        connection.query('SELECT * FROM products WHERE stock_quantity < 5', (error, products) =>
+        {
+            if (error) throw error;
+            if (products.length > 0) {
+                console.table(products);
+            } else {
+                console.log(chalk.yellow("Inventory is Full!"));
+            }
+            connection.end();
+        });
+    },
 }
